f5434d51522830a2e44028ff00ecd91414e86bbd
조민지 <jsh00999@naver.com>
2022-09-27 10:19:42 +0900

    0927과제

---
+++ src/Assign21.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//정숫값의 부호를 판정해서 표시.
+public class Assign21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean sw = true;
+
+        while (sw) {
+            System.out.println("정숫값:");
+            int num = scanner.nextInt();
+            if (num > 0) {
+                System.out.print("이 값은 양수입니다.");
+            } else if (num < 0) {
+                System.out.println("이 값은 음수입니다.");
+            } else {
+                System.out.println("이 값은 0입니다.");
+            }
+
+            System.out.print("다시 한번? 1-Yes/ 0-No:");
+            int answer = scanner.nextInt();
+            if (answer == 0) sw=false;
+
+        }
+    }
+}
