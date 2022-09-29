647bacedf56829ec4ff80dcc59e4a40dbe706eb2
minji <jsh00999@naver.com>
2022-09-29 21:11:36 +0900

    자바과제 0929

---
+++ src/Assign33.java
@@ -0,0 +1,19 @@
+//양의 정숫값을 0까지 카운트다운하는 프로그램(while문으로 작성)
+
+import java.util.Scanner;
+
+public class Assign33 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        do {
+            System.out.print("양의 정숫값:");
+            num = scanner.nextInt();
+        }while(num<=0);
+
+        while (num >= 0) {
+            System.out.println(num);
+            num--;
+        }
+    }
+}
