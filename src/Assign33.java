4d1334d83278bb5775d66e71b1bd35616b3c8551
minji <jsh00999@naver.com>
2022-09-29 21:13:02 +0900

    자바과제 0929

--- src/Assign33.java
+++ src/Assign33.java
@@ -6,6 +6,7 @@ public class Assign33 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num;
+        System.out.println("카운트다운합니다.");
         do {
             System.out.print("양의 정숫값:");
             num = scanner.nextInt();
